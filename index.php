a0b3e2584896a53f3ed43e92d16e8a2622cb5c83
Nestcraft <pradnyajadhav995@gmail.com>
2024-02-05 15:13:11 +0530

    added portfolio

---
+++ index.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pradnyashila - Developer Portfolio</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet"> -->
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/css/swiper.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/js/swiper.min.js"></script>
+</head>
+<body>
+    <header></header>
+    <main>
+        <div class="container contactinfo">
+            <div class="row">
+                <div class="col-md-5" style="margin-top:20px;">
+                    <div class="photo">
+                      <img src="images/profileimage.jpg" alt="profileimage" class="circular-image">
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="contact">
+                            <div class="container textchange">
+                            <span style="font-size:20px;">Hi, I'm </span></br>
+                            <h1 class="animate-charcter"> Pradnyashila Jadhav</h1></br>
+                                <span class="text first-text">I'm a</span>
+                                <span class="text sec-text"></span>
+                            </div>
+                            <!-- I'm passionate about learning new things and technologies</br> -->
+                            based in Airoli, Navi Mumbai </br></br>
+                            <a href="https://www.linkedin.com/in/software-developer-pradnyashila-jadhav" target="_blank"><img src="images/linkedin.png" style="width: 4.6%;
+                            " alt="LinkedIn"></a>
+                            <a href="https://github.com/pradnyashilaNavn/" target="_blank"><img src="images/git.jpeg" style="width: 4.6%;
+                            " alt="Git"></a>
+                            <a href="mailto:pradnyajadhav995@gmail.com" target="_blank"><img src="images/canvagmail.png" style="width: 5.6%;
+                            " alt="Gmail"></a>
+                            <a href="https://twitter.com/pradnya60214744" target="_blank"><img src="images/twitter.png" style="width: 4.6%;
+                            " alt="Twitter"></a>
+                            <a class="bn31" href="/" target="_blank"><span class="bn31span">Download Resume</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container about">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="skills">
+                        <h3>About</h3>
+                        <p>Develop and design responsive web apps. </br>Experienced working in PHP, MYSQL,
+                        and </br>Frameworks like Laravel, Codeigniter and responsive design.</br> Love to keep books 
+                        and read sometimes.</p>
+                        <h3>Ingredients</h3>
+                        <img src="images/skills/phpimage.svg"  alt="PHP" class="hover" title="PHP">
+                        <img src="images/skills/laravel.png"  alt="LARAVEL" class="hover" title="Laravel">
+                        <img src="images/skills/javascript.png"  alt="JAVASCRIPT" class="hover" title="JavaScript">
+                        <img src="images/skills/html5o.png"  alt="HTML5" class="hover" title="HTML5">
+                        <img src="images/skills/css3.jpg"  alt="CSS3" class="hover" title="CSS3">
+                        <img src="images/skills/mysql.png"  alt="MYSQL" class="hover" title="MySQL">
+                        <img src="images/skills/oops.png"  alt="OOPS" class="hover" title="OOPS">
+                        </br> <img src="images/skills/codeigniter.png"  alt="CODEIGNITER" class="hover" title="CodeIgniter">
+                        <img src="images/skills/vue-image.svg"  alt="VUEJS" class="hover" title="Vue.js">
+                        <img src="images/skills/jsonimage.png"  alt="JSON" class="hover" title="JSON">
+                        <img src="images/skills/git.png"  alt="GIT" class="hover" title="Git">
+                        <img src="images/skills/cromextensions.png" alt="CROMEXTENSIONS" class="hover" title="Chrome Extensions">
+                    </div>
+                </div>
+                <div class="col-md-6" style="background-color1:red;">
+                    <div class="banner">
+                        <video width="100%" height="100%" autoplay loop muted>
+                            <source src="images/type.mp4" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container projects">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="project">
+                        <h3>Accomplishment</h3>
+                        <section class="half-color-box1">
+                            <div class="container spacer por">
+                                <div class="text-whtie text-center ml-3">
+                                    <!-- <h1 class="bold">What Clients Say</h1>
+                                    <p class="lead">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, at!
+                                    </p> -->
+                                </div>
+                                <div class="swiper-container swiper-testimonial">
+                                    <div class="swiper-wrapper">
+                                        <div class="swiper-slide">
+                                            <div class="review-box" style="margin-top:-15px;">
+                                                <div class="media">
+                                                    <!-- <img class="mr-3" src="https://picsum.photos/50/50" alt="Generic placeholder image"> -->
+                                                    <div class="media-body">
+                                                        <h5>Project1 </h5>
+                                                        <!-- <p class="user-post">Carolina Music app</p> -->
+                                                        <span class="accent" data-v-a5e898ee="" style="background: -webkit-linear-gradient(315deg, #42d392 25%, #647eff); background-clip: text; 
+                                                        -webkit-background-clip: text; -webkit-text-fill-color: transparent; font-size: 25px;  font-weight: 600;">Carolina Music app</span>
+                                                    </div>
+                                                </div>
+                                                <p class="read">
+                                                    <b>Technologies:</b> HTML5, CSS3, JavaScript </br>
+                                                    <b>Description:</b> A music player using HTML CSS and js. This music player will have all the features found in a typical music player.
+                                                     You will be able to add, play, pause and seek songs that have been added. </br></br> 
+                                                    <div class="center-button">
+                                                        <a href="https://pradnyashilanavn.github.io/carolina.github.io/" target= _blank class="btn btn-danger btn-center" style="background-color:#ff1361;" role="button" aria-disabled="true">View more</a>
+                                                    </div>
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <div class="review-box" style="margin-top:-15px;">
+                                                <div class="media">
+                                                    <!-- <img class="mr-3" src="https://picsum.photos/50/50/" alt="Generic placeholder image"> -->
+                                                    <div class="media-body">
+                                                        <h5>Project2 </h5>
+                                                        <span class="accent" data-v-a5e898ee="" style="background: -webkit-linear-gradient(315deg, #42d392 25%, #647eff); background-clip: text; 
+                                                        -webkit-background-clip: text; -webkit-text-fill-color: transparent; font-size: 25px;  font-weight: 600;">PHP mini project</span>
+                                                        <!-- <p class="user-post">PHP mini project</p> -->
+                                                    </div>
+                                                </div>
+                                                <p class="read">
+                                                    <b>Technologies:</b> HTML5, CSS3, JavaScript, PHP, Mysqli, Ajax, SweetAlert </br>
+                                                    <b>Description:</b> I have developed a comprehensive CRUD application using Bootstrap 4, PHP, MySQLi, Ajax, and SweetAlert. 
+                                                    It allows users to add multiple entries, provides functionality for both active and inactive states, includes a search feature, 
+                                                    and implements pagination.</br></br>
+
+                                                    <div class="center-button">
+                                                        <a href="#" target= _blank class="btn btn-danger btn-center" style="background-color:#ff1361;" role="button" aria-disabled="true">View more</a>
+                                                    </div>
+                                                </p>
+                                                <img class="quote" src="img/icons/quote.svg" alt="">
+                                            </div>
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <div class="review-box" style="margin-top:-15px;">
+                                                <div class="media">
+                                                    <!-- <img class="mr-3" src="https://picsum.photos/50/50" alt="Generic placeholder image"> -->
+                                                    <div class="media-body">
+                                                        <h5> Project3</h5>
+                                                        <span class="accent" data-v-a5e898ee="" style="background: -webkit-linear-gradient(315deg, #42d392 25%, #647eff); background-clip: text; 
+                                                        -webkit-background-clip: text; -webkit-text-fill-color: transparent; font-size: 25px;  font-weight: 600;">PHP-OOP mini project</span>
+                                                        <!-- <p class="user-post">PHP-OOP mini project </p> -->
+                                                    </div>
+                                                </div>
+                                                <p class="read">
+                                                    <b>Technologies:</b> HTML5, CSS3, JavaScript, PHP, OOP, MVC, PDO, Ajax, SweetAlert </br>
+                                                    <b>Description:</b> I have developed a comprehensive CRUD application using Bootstrap 4, PHP-OOP (MVC), PDO-MySQL, Ajax, DataTable, SweetAlert, and Fontawesome!</br></br>
+                                                    <div class="center-button">
+                                                        <a href="http://phpoopmvc.infinityfreeapp.com/" target= _blank class="btn btn-danger btn-center" style="background-color:#ff1361;" role="button" aria-disabled="true">View more</a>
+                                                    </div>
+                                                </p>
+                                                <img class="quote" src="img/icons/quote.svg" alt="">
+                                            </div>
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <div class="review-box" style="margin-top:-15px;">
+                                                <div class="media">
+                                                    <!-- <img class="mr-3" src="https://picsum.photos/50/50" alt="Generic placeholder image"> -->
+                                                    <div class="media-body">
+                                                        <h5>Project4</h5>
+                                                        <span class="accent" data-v-a5e898ee="" style="background: -webkit-linear-gradient(315deg, #42d392 25%, #647eff); background-clip: text; 
+                                                        -webkit-background-clip: text; -webkit-text-fill-color: transparent; font-size: 25px;  font-weight: 600;">Vue.js mini project</span>
+                                                        <!-- <p class="user-post">Vue.js mini project</p> -->
+                                                    </div>
+                                                </div>
+                                                <p class="read">
+                                                    <b>Technologies:</b> Vue.js, Bootstrap 4, PHP, MySQLi and Axios.</br>
+                                                    <b>Description:</b> I am creating dialog pop-ups on the main page for adding, editing, and deleting users. 
+                                                    The backend, powered by PHP, manages requests, while the functionality of the CRUD application is implemented using Vue.js and the Axios library.</br></br>
+                                                    <div class="center-button">
+                                                        <a href="http://vuejs.rf.gd/" target= _blank class="btn btn-danger btn-center" style="background-color:#ff1361;" role="button" aria-disabled="true">View more</a>
+                                                    </div>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- Add Pagination --></br></br>
+                                    <div class="swiper-pagination"></div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container recommendations">
+        <h3>Recommendations</h3></br>
+            <div class="row">
+                <div class="col-md-6 text-center">
+                    <div class="recommendation">
+                    <div class="flip-card mx-auto">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <p class="recom" style="margin-top:15px;">She is kind hearted and nice, eager to attain new skills and good at 
+                                    recognizing a problem and working towards a fix.</br></br>
+                                </p>
+                                <div class="recomname">
+                                <span><b>- Mr. Harish Aiyer</b></span>
+                                </div>
+                            </div>
+                            <div class="flip-card-back">
+                                <p class="recom">
+                                <h2> Mr.Harish Chandrashekhar</h2>     
+                                Managing Director at Droid Innovations Pvt ltd</p> 
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-md-6 text-center">
+                    <div class="recommendations">
+                    <div class="flip-card mx-auto">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <p class="recom" style="margin-top:15px;">During the span, we found her punctual and hardworking.
+                                    She was conscientious and committed to her work. We wish
+                                    her very best in all her future assignment.</br></br> 
+                                </p>
+                                <div class="recomname">
+                                <span><b>- Mr. Anand Singh</b></span>
+                                </div>
+                            </div>
+                            <div class="flip-card-back"> 
+                                <p class="recom">
+                                   <h2>Mr.Anand Singh</h2> 
+                                   Managing Director at Nestcraft Design</p> 
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>                
+            </div>
+        </div>
+</br></br>
+</br>
+
+    </main>
+</body>
+</html>
+<script>
+    var swiper = new Swiper('.swiper-container.swiper-testimonial', {
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next-test',
+        prevEl: '.swiper-button-prev-test',
+    },
+    // autoplay: {
+    //         delay: 5000,
+    //         disableOnInteraction: false, 
+    //     },       
+});
+const text = document.querySelector(".sec-text");
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "Software Developer.";
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "Problem solver.";
+    }, 4000);
+    setTimeout(() => {
+        text.textContent = "Collaborative team player.";
+    }, 8000);
+}
+textLoad();
+setInterval(textLoad, 12000);
+</script>
